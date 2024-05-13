9a9081ec8607d8ed46f57dabd8b088a86756742f
Dainis <91558690+gatesbydude@users.noreply.github.com>
2024-05-13 11:30:38 +0300

    fully translated

--- src/casino/laimesti/laimestiDizloto.ts
+++ src/casino/laimesti/laimestiDizloto.ts
@@ -4,25 +4,25 @@ const laimestiDizloto: ChanceRecord = {
   win_massive: {
     chance: 0.01,
     reward: 5000,
-    name: '**!!! MILZĪGO !!!**',
+    name: '**!!! JACKPOT !!!**',
     color: 0xeb44ff,
   },
   win_big: {
     chance: 0.1,
     reward: 1200,
-    name: '**! LIELO !**',
+    name: '**! HUGE !**',
     color: 0x45e8e8,
   },
   win_medium: {
     chance: 0.2,
     reward: 700,
-    name: '**vidējo**',
+    name: '**average**',
     color: 0x69ee54,
   },
   win_small: {
     chance: 0.45,
     reward: 350,
-    name: 'mazo',
+    name: 'small',
     color: 0xf6d179,
   },
   lose: {
